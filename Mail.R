58973db04c57ac0d95123c3629b4b8d020cf89a3
Kulkarni Mahesh <KulkarniMahesh@JohnDeere.com>
2020-05-22 18:40:17 +0530

    Third Commit

--- Mail.R
+++ Mail.R
@@ -10,6 +10,10 @@ img_file_path <-
     package = "blastula"
   )
 
+file = "EmailModule.pptx"
+# ppt_path<- system.file(
+#   "Email_Module.pptx", package = "blastula"
+# )
 img_string <- add_image(file = img_file_path)
 
 email <-
@@ -22,7 +26,8 @@ This Mail is from Rstudio. Created by Mahesh Kulkarni.
 I am working on this module.I create one script which sends a 
 Mail. So the first task is can we sent a mail from project ?
 Ans: Yes
-"
+",
+img_string
       )
     ),
     footer = md(
@@ -32,22 +37,35 @@ Ans: Yes
     )
   )
 
+attach<- add_attachment(
+  email = email,
+  file = file,
+  content_type = mime::guess_type(file),
+  filename = basename(file)
+)
 # shejawalamola@johndeere.com
 
-creds(user = NULL, provider = NULL, host = NULL, port = NULL, use_ssl = TRUE)
+# creds(user = NULL, provider = NULL, host = NULL, port = NULL, use_ssl = TRUE)
 
 create_smtp_creds_file(file = "email_creds",
-                       user = "kulkarnimahesh@johndeere.com",
+                       user = "vmk80555@outlook.com",
                        provider = "office365",
-                       host = "smtps://outlook.office365.com.office.johndeere.myshn.net/mapi/emsmdb/?MailboxId=829b695b-ed9a-4905-9d6a-c7e7f9c0fb8a@johndeere.com",
-                       port = 465,
+                       host = "smtp.office365.com",
+                       port = 587,
                        use_ssl = TRUE)
 
+# smtp <- create_smtp_creds_key(
+#   id = "outlook",
+#   user = "kulkarnimahesh@johndeere.com",
+#   provider = "office365",
+#   host = "smtp.office365.com",
+#   use_ssl = TRUE)
+
 # Sending email by SMTP using a credentials file
-email %>%
+attach %>%
   smtp_send(
-    to = "malirohit@johndeere.com",
-    from = "kulkarnimahesh@johndeere.com",
+    to = "amol.shejawal@gmail.com",
+    from = "vmk80555@outlook.com",
     subject = "This Mail From Rstudio Mahesh's Project",
     credentials = creds_file("email_creds")
   )
